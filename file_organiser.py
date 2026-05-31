7290a7e1b74ea32aca0f87a53fff3908d865a662
vashurajotia <vrajotia2@gmail.com>
2026-05-31 22:59:32 +0530

    full

--- file_organiser.py
+++ file_organiser.py
@@ -15,7 +15,26 @@ FILE_TYPES = {
 
 # create folders if they don't exist
 for folder in FILE_TYPES.keys():  # creating folders for each file type
-    folder_path = os.path.join(FOLDER_PATH, folder) 
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
+    folder_path = os.path.join(FOLDER_PATH, folder)    # creating the path for the folder
 
+
+    if not os.path.exists(folder_path):  # checking if the folder already exists
+
+
+        os.makedirs(folder_path) # creating the folder if it doesn't exist
+
+# organize files
+for file in os.listdir(FOLDER_PATH):
+    file_path = os.path.join(FOLDER_PATH, file)
+
+    #skip folders
+    if os.path.isdir(file_path):
+        continue
+    # get file extension
+    file_ext = os.path.splitext(file)[1].lower()
+
+    for folder, extensions in FILE_TYPES.items():
+        if file_ext in extensions:
+            shutil.move(file_path, os.path.join(FOLDER_PATH, folder, file))
+
+print("Files organized successfully!")
